8f25d8ec4a30ff7b4c5d88b81e40da6b8aad7ed6
Victor Fernandez <victor.j.fdez@gmail.com>
2016-09-23 20:25:37 +0000

    corrected config used

--- index.js
+++ index.js
@@ -3,9 +3,13 @@
 var url = require('url');
 
 const PostAsset = hexo.model('PostAsset');
-//override default asset_img 
+/*
+ * Override asset_img tag function with this function. This function
+ * does the exact same function as the core asset_img tag, but it also
+ * allows for url override 
+ */
 hexo.extend.tag.register('asset_img', function(args) {
-  const assetURL = hexo.config.cdn_url;
+  const assetURL = hexo.config.asset_img_url;
   var url = '';
 
   var slug = args.shift();
@@ -21,7 +25,8 @@ hexo.extend.tag.register('asset_img', function(args) {
   var title = args.length ? args.join(' ') : '';
   // alt always exist
   var alt = title || asset.slug;
-  hexo.log.debug(`generating new url ${assetURL}${hexo.config.root}${asset.path}`);
+
+  hexo.log.debug(`[hexo-tag-asset-img] override asset_img generating img tag with url: ${assetURL}${hexo.config.root}${asset.path}`);
 
   return '<img src="' + url + hexo.config.root + asset.path + '" alt="' + alt + '" title="' + title + '">';
 });
