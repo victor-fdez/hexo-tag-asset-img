a07a985d4a793de7a0a914d3f553aa8caa2cb207
Victor Fernandez <victor.j.fdez@gmail.com>
2016-09-23 03:16:46 +0000

    initialize repo

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var url = require('url');
+
+const PostAsset = hexo.model('PostAsset');
+//override default asset_img 
+hexo.extend.tag.register('asset_img', function(args) {
+		const cdnUrl = hexo.config.cdn_url;
+		var url = '';
+
+    var slug = args.shift();
+    if (!slug) return;
+
+    var asset = PostAsset.findOne({post: this._id, slug: slug});
+    if (!asset) return;
+
+		if (cdnUrl)
+			url = cdnUrl; 
+
+    // if title is not assigned, set it ''
+    var title = args.length ? args.join(' ') : '';
+    // alt always exist
+    var alt = title || asset.slug;
+		hexo.log.debug(`generating new url ${cdnUrl}${hexo.config.root}${asset.path}`);
+
+    return '<img src="' + url + hexo.config.root + asset.path + '" alt="' + alt + '" title="' + title + '">';
+});
